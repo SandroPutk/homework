dc404b25e8bf786d32b9203a8818249cf6320ae5
SandroPutk <103522564+SandroPutk@users.noreply.github.com>
2022-09-20 10:53:03 +0400

    Add files via upload

---
+++ FirstLesson.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 21H2v2
+ */
+import java.io.*;
+import java.util.Scanner;
+public class NewClass {
+    public static void main(String[] args)
+    {
+        // კონსოლიდან შეტანა
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name=scan.nextLine();
+        System.out.println("hello" + name);
+        
+        /*
+        კონსოლიდან მონაცემების შეტანა და შედეგების გამოტანა
+        */
+           
+        System.out.println("Hello World");
+    int a= 2;
+    int b=3;
+    System.out.println("a*b=" +a+"*"+b+"="+(a*b));
+        
+        /*
+    System.out.println("Hello World");
+    int a= 2;
+    int b= 2+3;
+    int c = 5;
+    // shedareba a da b == operatorit
+    System.out.println(" Result " + a + " and " + b
+            + " equal" + ( a == b));
+    
+    
+    // shedareba b da c is == operatorit
+    System.out.println("result " + b + "and" + c 
+            + "equal" + (b == c));
+    
+    System.out.println("result"  + a + "and " + b + "equal" 
+            + c + "resulttwo" + (a == b) & ( b == c));
+        
+*/
+     
+    }
+    
+}
